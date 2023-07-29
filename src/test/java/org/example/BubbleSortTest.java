c18ac8c1bf9c70e135f08a0de44a2c768db4791c
Emmalee Liao <s3950278@student.rmit.edu.au>
2023-07-29 11:25:26 +1000

    change test input value

--- src/test/java/org/example/BubbleSortTest.java
+++ src/test/java/org/example/BubbleSortTest.java
@@ -6,10 +6,10 @@ import static org.junit.jupiter.api.Assertions.*;
 public class BubbleSortTest {
     @Test
     void name() {
-        int[] input = { 64, 34, 25, 12, 22, 11, 90 };
+        int[] input = { 6, 3, 7, 8, 9 };
         BubbleSort bubbleSort = new BubbleSort();
         int[] sorted_arr = bubbleSort.bubble_sort(input);
-        int[] expected = {11, 12, 22, 25, 34, 64, 90};
+        int[] expected = {3, 6, 7, 8, 9};
 
         for (int i=0; i< input.length; i++){
             assertEquals(expected[i], sorted_arr[i], "Error: found index " + i + " is not sorted");
